fe1032c2f4198cfd26c7d6bff1884ea0f7108187
Sunny Nguyen <thuylinh2409nguyen@gmail.com>
2023-11-28 09:53:44 +0700

    Update multiple environment

--- src/main/java/commons/BaseTest.java
+++ src/main/java/commons/BaseTest.java
@@ -1,14 +1,26 @@
 package commons;
 
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import io.restassured.RestAssured;
 
 public class BaseTest {
+	
+	@Parameters({"environment"})
 	@BeforeTest
-	public void beforeAll() {
-		RestAssured.baseURI = "http://localhost";
-        RestAssured.basePath = "/student";
-        RestAssured.port = 8080;
+	public void beforeAll(String environment) throws Exception {
+		switch (environment) {
+		case "dev":
+			RestAssured.baseURI = GlobalConstants.BASE_URL_DEV;
+			break;
+		case "test":
+			RestAssured.baseURI = GlobalConstants.BASE_URL_TEST;
+			break;
+		default:
+			throw new Exception("Please enter the valid environment");
+		}
+        RestAssured.basePath = GlobalConstants.BASE_PATH;
+        RestAssured.port = GlobalConstants.BASE_PORT;
 	}
 }
---
+++ src/main/java/commons/GlobalConstants.java
@@ -0,0 +1,10 @@
+package commons;
+
+public class GlobalConstants {
+	public static final String BASE_URL_DEV = "http://localhost";
+	public static final String BASE_URL_TEST = "https://reqres.in/";
+	public static final String BASE_PATH = "/student";
+	public static final int BASE_PORT = 8080;
+	
+	
+}
